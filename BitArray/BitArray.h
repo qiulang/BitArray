e0c9d589c1d5a93852dab8ec3dafce0473641a3b
qiulang <qiulang2000@hotmail.com>
2016-01-29 13:34:44 +0800

    Initial Commit

---
+++ BitArray/BitArray.h
@@ -0,0 +1,19 @@
+//
+//  BitArray.h
+//  BitArray
+//
+//  Created by 邱 朗 on 16/1/29.
+//  Copyright © 2016年 邱 朗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitArray.
+FOUNDATION_EXPORT double BitArrayVersionNumber;
+
+//! Project version string for BitArray.
+FOUNDATION_EXPORT const unsigned char BitArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitArray/PublicHeader.h>
+
+
